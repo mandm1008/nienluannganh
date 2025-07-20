15e6f2f1f7490ab6ac813a69ba8d8b2f69f964e7
mandm1008 <dominhman311@gmail.com>
2025-07-20 05:43:22 +0000

    add session user

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/Header';
+import SessionWrapper from '@/components/SessionWrapper';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -23,8 +24,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header />
-        {children}
+        <SessionWrapper>
+          <Header />
+          {children}
+        </SessionWrapper>
       </body>
     </html>
   );
